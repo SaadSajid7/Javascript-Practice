24e888bae01dae4fc351ff4fe62093cf8769b38f
SaadSajid7 <sajidsaad745@gmail.com>
2022-11-01 00:30:17 +0500

    Chapter 1 to 4 Completed

---
+++ Assignment 1 (Ch 1-8)/app.js
@@ -0,0 +1,37 @@
+// Chapter: 1 - Alert
+// alert("Thanks for your input!");
+
+// Chapter: 2 - Variable for Strings
+var name = "Mark";
+console.log(name);
+
+var nationality;
+nationality = "U.S.";
+console.log(nationality);
+
+var thanx = "Thanks You!"
+// alert(thanx);
+
+// Chapter: 3 - Variable for Numbers
+var originalNum = 25;
+var newNum = originalNum + 7;
+console.log(newNum);
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum);
+
+var originalNum = "23"; // 23 is a string
+var newNum = originalNum + 7;
+console.log(newNum);  // Result = 237
+
+// Chapter: 4 - Variable Names Legal and Illegal
+// var abc         // legal
+// var ab12        // legal
+// var a_b         // legal
+// var @abc/ab@    // illegal because of special character
+// var var         // illegal because of keywords
+// var 12abc       // illegal because of number start
+// var a+-*/b      // illegal because of arithmetic operators
+// var 123         // illegal because of numbers
