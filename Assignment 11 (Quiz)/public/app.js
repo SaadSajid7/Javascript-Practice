d9a14abe1a3d347fe8caf0c49b853bf6d465c744
Ahmed Sajid <sajidsaad745@gmail.com>
2022-12-17 18:02:31 +0500

    Quiz App

---
+++ Assignment 11 (Quiz)/public/app.js
@@ -0,0 +1,50 @@
+var question1 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question2 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question3 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question4 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question5 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question6 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question7 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question8 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question9 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
+var question10 = {
+  question: "HTML stands for: ",
+  options: ['Hyper Text Markup Language', 'Hexa Text Markup Language', 'Hexa Text Mockup Language', 'Hyper Text Mockup Language'],
+  correctAnswer: 'Hyper Text Markup Language'
+}
